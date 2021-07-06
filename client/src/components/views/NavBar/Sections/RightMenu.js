c0a34d22615b13d9e281886d556b90706589b041
giu0213@naver.com <giu0213@naver.com>
2021-07-06 23:11:13 +0900

    navbar login user setting

--- client/src/components/views/NavBar/Sections/RightMenu.js
+++ client/src/components/views/NavBar/Sections/RightMenu.js
@@ -36,6 +36,9 @@ function RightMenu(props) {
         <Menu.Item key="logout">
           <a onClick={logoutHandler}>Logout</a>
         </Menu.Item>
+        <Menu.Item key="upload">
+          <a href="/vidio/upload">Video Upload</a>
+        </Menu.Item>
       </Menu>
     )
   }
