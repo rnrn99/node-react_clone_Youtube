e050946e4122a365c27fd28944f73fb23d22806b
giu0213@naver.com <giu0213@naver.com>
2021-08-13 16:08:53 +0900

    [ADD] UploadPage setting

--- client/src/components/views/UploadPage/UploadPage.js
+++ client/src/components/views/UploadPage/UploadPage.js
@@ -1,11 +1,120 @@
-import React from 'react'
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import { Typography, Button, Form, Input } from "antd";
+import { PlusOutlined } from "@ant-design/icons";
+
+const { Title } = Typography;
+const { TextArea } = Input;
+
+const PrivateOption = [
+  { value: 0, label: "Private" },
+  { value: 1, label: "Public" },
+];
+
+const CategoryOption = [
+  { value: 0, label: "Film & Animation" },
+  { value: 1, label: "Autos & Vehicles" },
+  { value: 2, label: "Music" },
+  { value: 3, label: "Pets & Animals" },
+];
 
 function UploadPage() {
-    return (
-        <div>
-            Upload Video Here!
+  const [VideoTitle, setVideoTitle] = useState("");
+  const [Description, setDescription] = useState("");
+  const [Private, setPrivate] = useState(0);
+  const [Category, setCategory] = useState("Film & Animation");
+
+  const onTitleChange = (event) => {
+    setVideoTitle(event.currentTarget.value);
+  };
+
+  const onDescriptionChange = (event) => {
+    setDescription(event.currentTarget.value);
+  };
+
+  const onPrivateChange = (event) => {
+    setPrivate(event.currentTarget.value);
+  };
+
+  const onCategoryChange = (event) => {
+    setCategory(event.currentTarget.value);
+  };
+
+  return (
+    <div style={{ maxWidth: "700px", margin: "2rem auto" }}>
+      <div style={{ textAlign: "center", marginBottom: "2rem" }}>
+        <Title level={2}>Upload Video</Title>
+      </div>
+
+      <Form onSubmit>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <Dropzone onDrop multiple maxSize>
+            {({ getRootProps, getInputProps }) => (
+              <div
+                style={{
+                  width: "300px",
+                  height: "240px",
+                  border: "1px solid lightgray",
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+                {...getRootProps}
+              >
+                <input {...getInputProps()} />
+                <PlusOutlined style={{ fontSize: "3rem" }} />
+              </div>
+            )}
+          </Dropzone>
+
+          <div>
+            <img src alt />
+          </div>
         </div>
-    )
+
+        <br />
+        <br />
+
+        <label>Title</label>
+        <Input onChange={onTitleChange} value={VideoTitle} />
+
+        <br />
+        <br />
+
+        <label>Description</label>
+        <TextArea onChange={onDescriptionChange} value={Description} />
+
+        <br />
+        <br />
+
+        <select onChange={onPrivateChange}>
+          {PrivateOption.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+
+        <br />
+        <br />
+
+        <select onChange={onCategoryChange}>
+          {CategoryOption.map((item, index) => (
+            <option key={index} value={item.value}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+
+        <br />
+        <br />
+
+        <Button type="primary" sizr="large" onClick>
+          Submit
+        </Button>
+      </Form>
+    </div>
+  );
 }
 
-export default UploadPage
+export default UploadPage;
