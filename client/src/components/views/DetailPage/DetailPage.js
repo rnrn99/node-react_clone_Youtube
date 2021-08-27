7ccef1de50f14eff72a3e5a9b8b98f556875b13d
giu0213@naver.com <giu0213@naver.com>
2021-08-27 16:55:16 +0900

    [MOD] remove subscription button to yourself

--- client/src/components/views/DetailPage/DetailPage.js
+++ client/src/components/views/DetailPage/DetailPage.js
@@ -22,6 +22,14 @@ function DetailPage(props) {
     });
   }, []);
   if (VideoDetail.writer) {
+    const subscribeButton = VideoDetail.writer._id !==
+      localStorage.getItem("userId") && (
+      <Subscribe
+        userTo={VideoDetail.writer._id}
+        userFrom={localStorage.getItem("userId")}
+      />
+    );
+
     return (
       <Row gutter={[16, 16]}>
         <Col lg={18} xs={24}>
@@ -31,14 +39,7 @@ function DetailPage(props) {
               src={`http://localhost:5000/${VideoDetail.filePath}`}
               controls
             />
-            <List.Item
-              actions={[
-                <Subscribe
-                  userTo={VideoDetail.writer._id}
-                  userFrom={localStorage.getItem("userId")}
-                />,
-              ]}
-            >
+            <List.Item actions={[subscribeButton]}>
               <List.Item.Meta
                 avatar={<Avatar src={VideoDetail.writer.image} />}
                 title={VideoDetail.writer.name}
